4805da15ab38f7ff229329a8a4f36dc64796fa72
zh <2269679488@qq.com>
2023-01-01 20:57:41 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+import java.net.SocketTimeoutException;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
